9d958fea28ecd7cea5db38e42004637467dc265c
Zsalvador87 <zsalvadornekoskate@gmail.com>
2025-06-06 20:34:29 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -3,17 +3,60 @@
 <head>
     <meta charset="UTF-8">
     <title>Iniciar sesión</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+        }
+        form {
+            background-color: #4ebd46 ;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            text-align: center;
+        }
+        h2 {
+            margin-bottom: 20px;
+        }
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-top: 5px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        button {
+            padding: 10px 20px;
+            border: none;
+            background-color: #007bff;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
-    <h2>Login</h2>
     <form action="verificar_login.php" method="POST">
+        <h2>Login</h2>
         <label for="nombre_usuario">Usuario:</label><br>
-        <input type="text" name="nombre_usuario" required><br><br>
+        <input type="text" name="nombre_usuario" required><br>
 
         <label for="password">Contraseña:</label><br>
-        <input type="password" name="password" required><br><br>
+        <input type="password" name="password" required><br>
 
         <button type="submit">Ingresar</button>
     </form>
 </body>
 </html>
+
+
